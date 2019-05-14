42d3ab67d32a28aed44e0c0403f361a75f6f6610
Paphiopedilum <48099895+Paphiopedilum@users.noreply.github.com>
2019-05-14 21:00:59 +0200

    MAJ

--- ProjetJSYNorvick/script.js
+++ ProjetJSYNorvick/script.js
@@ -6,31 +6,25 @@ $(document).ready(function() {
 
     var id = $(this).attr("id");
 
-
     if (openedWidgets.indexOf(id) > -1) {
 
       $("#" + id + "_widget").remove();
+      openedWidgets.splice(openedWidgets.indexOf(id), 1);}
 
-      openedWidgets.splice(openedWidgets.indexOf(id), 1);
+      else {
 
-
-    } else {
       var url = "widgets/" + id.toLowerCase() + "_widget.html";
 
       $('#widgets').append($('<div></div>').load(url));
-
       openedWidgets.push(id);
-
     }
-  
+
     console.log(openedWidgets);
   });
-
   $("#widgets").draggable(function() {
 
     var id = $(this).attr("id");
 
     $("#" + id + "_widget").draggable();
-
   });
 });
